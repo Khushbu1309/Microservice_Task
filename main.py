124a48fd22e02536031d3c7c9952322bffafbb56
Khushbu <74493334+Khushbu1309@users.noreply.github.com>
2023-05-05 02:39:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from func import *
 url = "https://example.com/"
 result = count_words(url)
 print(result)
+
+word_counts = count_words(url)
+with open('output.json', 'w') as f:
+     f.write(word_counts)
